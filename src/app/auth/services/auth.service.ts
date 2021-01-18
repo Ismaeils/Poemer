f353b08bb056e877a95cdfbc2451308ed34ce8cb
Ismaeil Ghouneim <Is.ghouneim@gmail.com>
2021-01-18 23:11:32 +0200

    Fixed spelling

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -28,7 +28,7 @@ export class AuthService{
                 
             }
         }
-        this.http.get('https://peomerapi.herokuapp.com/api/auth/google', options).subscribe(res=> console.log(res));
+        this.http.get('https://poemerapi.herokuapp.com/api/auth/google', options).subscribe(res=> console.log(res));
     }
     
 }
